dbd66638a8fbc1cda2230f25ea5e8cb26a21b2cb
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-10-31 19:39:30 +0900

    [frontend] サブディレクトリー以下にもサイト配置できるようにした

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -1,18 +1,18 @@
 // 各種プラグインのロード
-const fs = require("fs");
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const AutoPrefixer = require("autoprefixer");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const TerserJSPlugin = require("terser-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const globule = require("globule");
+const fs = require('fs');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const AutoPrefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const globule = require('globule');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 // ビルド先パス
-const DEST_PATH = path.join(__dirname, "./public");
+const DEST_PATH = path.join(__dirname, './public');
 
 // エントリーポイントに含めるパターン
 const getEntriesList = (targetTypes) => {
@@ -42,7 +42,7 @@ const getDynamicContentsJsonFiles = () => {
 const dynamicContentsJsonFiles = getDynamicContentsJsonFiles();
 let dynamicContents = {};
 for (const jsonFile of dynamicContentsJsonFiles) {
-  const jsonText = fs.readFileSync(jsonFile, "utf-8");
+  const jsonText = fs.readFileSync(jsonFile, 'utf-8');
   const json = JSON.parse(jsonText);
   Object.assign(dynamicContents, json);
 }
@@ -52,15 +52,15 @@ const app = {
   entry: Object.assign(
     {
       app: [
-        "./src/js/main.js",
-        "./src/scss/style.scss",
+        './src/js/main.js',
+        './src/scss/style.scss',
       ],
     },
   ),
   output: {
     path: path.resolve(__dirname, DEST_PATH),
-    filename: "js/[name].min.js",
-    publicPath: "/",
+    filename: 'js/[name].min.js',
+    publicPath: '',
   },
   module: {
     rules: [
@@ -68,10 +68,10 @@ const app = {
         test: /\.ejs$/,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
           },
           {
-            loader: "ejs-html-loader",
+            loader: 'ejs-html-loader',
             options: {
               // JSON動的コンテンツをEJSにインジェクションする
               contents: dynamicContents,
@@ -82,23 +82,23 @@ const app = {
       {
         test: /\.js?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.scss$/,
         use: [{
           loader: MiniCssExtractPlugin.loader,
         }, {
-          loader: "css-loader",
+          loader: 'css-loader',
         }, {
-          loader: "postcss-loader",
+          loader: 'postcss-loader',
           options: {
             plugins: [
               AutoPrefixer(),
             ],
           },
         }, {
-          loader: "sass-loader",
+          loader: 'sass-loader',
         }],
       },
       {
@@ -114,22 +114,22 @@ const app = {
     ],
   },
   resolve: {
-    extensions: [".js"],
+    extensions: ['.js'],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: "css/style.min.css",
+      filename: 'css/style.min.css',
     }),
     new CopyWebpackPlugin(
       [
         {
-          from: "**/*",
-          to: "img",
+          from: '**/*',
+          to: 'img',
         },
       ],
       {
-        context: "src/img",
+        context: 'src/img',
       },
     ),
   ],
@@ -146,7 +146,7 @@ const app = {
 };
 
 // EJSの変換定義を現在存在するファイル分だけ自動的に追加
-for (const [targetName, srcName] of Object.entries(getEntriesList({ejs: "html"}))) {
+for (const [targetName, srcName] of Object.entries(getEntriesList({ejs: 'html'}))) {
   app.plugins.push(new HtmlWebpackPlugin({
     filename : targetName,
     template : srcName,
